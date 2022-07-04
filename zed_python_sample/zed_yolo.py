ee5a883843b4ad9f58d78b01c4f98b912b461999
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-07-04 12:26:50 +0530

    Update zed_yolo.py

--- zed_python_sample/zed_yolo.py
+++ zed_python_sample/zed_yolo.py
@@ -9,12 +9,10 @@ import sys
 import time
 import logging
 
-
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 def main(argv) :
-
     config_path = "yolov4.cfg"
     weight_path = "yolov4.weights"
     meta_path = "coco.names"
@@ -54,19 +52,16 @@ def main(argv) :
         # Launch camera by id
         input_type.set_from_camera_id(zed_id)
 
+    # Create a ZED camera object
+    zed = sl.Camera()
+
+    # Set configuration parameters
+    input_type = sl.InputType()
+
     init = sl.InitParameters(input_t=input_type)
     init.camera_resolution = sl.RESOLUTION.HD1080
     init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
     init.coordinate_units = sl.UNIT.MILLIMETER
-
-    # Create a ZED camera object
-    zed = sl.Camera()
-    if not zed.is_opened():
-        log.info("Opening ZED Camera...")
-    status = zed.open(init)
-    if status != sl.ERROR_CODE.SUCCESS:
-        log.error(repr(status))
-        exit()
 
     # Open the camera
     err = zed.open(init)
@@ -74,7 +69,6 @@ def main(argv) :
         print(repr(err))
         zed.close()
         exit(1)
-
   
     # Set runtime parameters after opening the camera
     runtime = sl.RuntimeParameters()
@@ -90,17 +84,21 @@ def main(argv) :
     depth_image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
     point_cloud = sl.Mat()
 
+    weightsPath_tiny = "yolov4.weights"
+    configPath_tiny = "yolov4.cfg"
+
     net = cv2.dnn.readNetFromDarknet(config_path, weight_path)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
     model = cv2.dnn_DetectionModel(net)
     
+ 
     
     def YOLOv4_video(pred_image):
         model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
         image_test = cv2.cvtColor(pred_image, cv2.COLOR_RGBA2RGB)
         image = image_test.copy()
-        print('image',image.shape)
+        # print('image',image.shape)
         confThreshold= 0.6
         nmsThreshold = 0.4
         classes, confidences, boxes = model.detect(image, confThreshold, nmsThreshold)
@@ -113,9 +111,14 @@ def main(argv) :
     with open(meta_path, 'r') as f:
         LABELS = [cname.strip() for cname in f.readlines()]
 
+    COLORS = [[0, 0, 255], [30, 255, 255], [0,255,0]]
 
-    COLORS = [[0, 0, 255]]
-    while key != 113 :
+    frame_count = 0
+
+    exit_flag = True
+
+    while(exit_flag == True):
+        print("FRAME ", frame_count)
         err = zed.grab(runtime)
         if err == sl.ERROR_CODE.SUCCESS :
             # Retrieve the left image, depth image in the half-resolution
@@ -136,23 +139,29 @@ def main(argv) :
                 
                 x = int(left + width/2)
                 y = int(top + height/2)
+
                 color = COLORS[0]
-                img =cv2.rectangle(image_ocv,start_pooint,end_point,color,3)
-                img = cv2.circle(img,(x,y),5,[0,0,255],5)
+
+                img =cv2.rectangle(image_ocv,start_pooint,end_point,COLORS[1],2)
+                img = cv2.circle(img,(x,y),5,COLORS[1],5)
                 text = f'{LABELS[cl]}: {score:0.2f}'
-                cv2.putText(img,text,(int(left),int(top-7)),cv2.FONT_ITALIC,1,COLORS[0],2 )
+                cv2.putText(img,text,(int(left),int(top-7)),cv2.FONT_HERSHEY_COMPLEX,1,COLORS[0],2 )
                 
                 x = round(x)
                 y = round(y)
                 err, point_cloud_value = point_cloud.get_value(x, y)
-                distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] + point_cloud_value[1] * point_cloud_value[1] +  point_cloud_value[2] * point_cloud_value[2])
+                distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] + point_cloud_value[1] * point_cloud_value[1] + point_cloud_value[2] * point_cloud_value[2])
 
                 print("Distance to Camera at (class : {0}, score : {1:0.2f}): distance : {2:0.2f} mm".format(LABELS[cl], score, distance), end="\r")
-                cv2.putText(img,"Distance: "+str(round(distance/1000,2))+'m',(int(int(left+width)-180),int(int(top+height)+30)),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),1)
+
+                cv2.putText(img,"Distance: "+str(round(distance/1000,2))+'m',(int(int(left)),int(int(top)+30)),cv2.FONT_HERSHEY_COMPLEX,1,COLORS[2],2)
                 
                 cv2.imshow("Image", img)
+
+            frame_count = frame_count + 1
             
-            key = cv2.waitKey(1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                exit_flag = False
 
     cv2.destroyAllWindows()
     zed.close()
